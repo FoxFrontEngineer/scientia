d881363c5f027b7e6d65e549cb5e5896bae83f05
artem.bashkatoff@yandex.ru <a.bashkatov@t1-group.ru>
2021-03-14 19:19:08 +0300

    logout: added logout component and functional

---
+++ src/components/modules/Logout/Logout.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import Button from '@material-ui/core/Button';
+import { useAuth } from "../../../contexts/AuthContext";
+import Alert from '@material-ui/lab/Alert';
+import AlertTitle from "@material-ui/lab/AlertTitle";
+
+import { useHistory } from "react-router-dom"
+
+export default function Logout() {
+    const { currentUser, logout } = useAuth()
+    const [error, setError] = useState(null)
+    const history = useHistory()
+
+    async function handleLogout() {
+        try {
+            const response = await logout();
+            history.push("/login")
+        } catch (e) {
+            setError(e)
+        }
+    }
+
+    return (
+        <>
+            {error && <Alert severity="error">
+                <AlertTitle>Error</AlertTitle>
+                This is an error alert — <strong>{error}</strong>
+            </Alert>}
+            <Button  type="submit" onClick={handleLogout}>Выйти</Button>
+        </>
+
+    )
+
+}
--- src/components/modules/MainScreen/MainScreen.js
+++ src/components/modules/MainScreen/MainScreen.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React from "react";
+
+import Logout from "../Logout/Logout";
 
 export default function MainScreen() {
 
     return (
+        <>
         <h1>This is Main Screen after Login</h1>
+        <Logout />
+        </>
     )
 }
