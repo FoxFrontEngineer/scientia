480d6ba0fa7b52dfe523f7c701750391c656eb42
Башкатов Артем <a.bashkatov@t1-group.ru>
2021-05-10 16:45:23 +0300

    Added MenuSidebart in left side to navigate throught app

--- src/components/modules/MainScreen/MainScreen.js
+++ src/components/modules/MainScreen/MainScreen.js
@@ -1,13 +1,12 @@
 import React from "react";
-
-import Logout from "../Logout/Logout";
+import MenuSidebar from "components/ui/MenuSidebar";
 
 export default function MainScreen() {
 
     return (
         <>
         <h1>This is Main Screen after Login</h1>
-        <Logout />
+        <MenuSidebar />
         </>
     )
 }
